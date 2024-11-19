01f6e6337642b501eda0b4e957a6a0c8902db927
Mariia Chedryk <mary.chedryk@gmail.com>
2024-11-19 16:52:07 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+//*\\\\\\\\\\\\\\\\\\\\\\\ Task 1 \\\\\\\\\\\\\\\\\\\\\\\
+const task1 = '==========1========';
+console.log(task1);
+
+let num = 1;
+while (num < 11) {
+    console.log(num);
+    num += 1;
+}
+
+//*\\\\\\\\\\\\\\\\\\\\\\\ Task 2 \\\\\\\\\\\\\\\\\\\\\\\
+const task2 = '=============2===========';
+console.log(task2);
+
+const number = 20;
+for (let i = 0; i <= number; i ++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i); 
+}
+
+//*\\\\\\\\\\\\\\\\\\\\\\\ Task 3 \\\\\\\\\\\\\\\\\\\\\\\
+const task3 = '============3===========';
+console.log(task3);
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`2 × ${i} = ${2 * i}`);
+}
+//*\\\\\\\\\\\\\\\\\\\\\\\ Task 4 \\\\\\\\\\\\\\\\\\\\\\\
+const task4 = '===========4==========';
+console.log(task4);
+
+let massifs = [1, 2, 3, 4, 5];
+while (massifs[0], massifs[1],massifs[2], massifs[3]) {
+  console.log(massifs);
+break
+}
+//*\\\\\\\\\\\\\\\\\\\\\\\ Task 5 \\\\\\\\\\\\\\\\\\\\\\\
+const task5 = '============5===========';
+console.log(task5);
+
+const arrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let array of arrays) {
+  if (array === 7) {
+    break;
+ }
+  console.log(array);
+  
+}
+//*\\\\\\\\\\\\\\\\\\\\\\\ Task 6 \\\\\\\\\\\\\\\\\\\\\\\
+const task6 = '==========6=============';
+console.log(task6);
+
+
+for (let index = 0; index <= 37;) {
+  console.log(index);
+  index += 1;
+  if (index === 37) {
+    break;
+  }
+  
+}
+
+//*\\\\\\\\\\\\\\\\\\\\\\\ Task 7 \\\\\\\\\\\\\\\\\\\\\\\
+const task7 = '==========7=============';
+console.log(task7);
+
+let i = 1;
+while (i <= 20) {
+  if (i % 3 === 0) {
+    i++;
+    continue;
+  }
+  console.log(i);
+  i++;
+}
